c327faf7e93582585b67f3bb94751916cf7f81b3
renatoelho <renatoelho@gmail.com>
2023-09-06 00:28:58 -0300

    Update repo 20230905200354...

--- app/app.py
+++ app/app.py
@@ -1,18 +1,49 @@
+from io import BytesIO
+from time import sleep
 
+from PIL import Image
 import streamlit as st
+import pytesseract as ocr
 
 from views.config_page import set_page_config
 from views.content_hide import content_hide_streamlit
 
 
-if __name__ == "__main__":
+set_page_config()
+st.markdown(content_hide_streamlit, unsafe_allow_html=True) 
 
-    set_page_config()
-    st.markdown(content_hide_streamlit, unsafe_allow_html=True) 
+st.markdown(
+    """
+    # Converta suas imagens em texto com Python e Tesseract!
+    """
+)
 
-    st.markdown(
-        """
-        # Converta suas imagens em texto com Python e Tesseract!
-        """
+def convert_image_to_text(input_imagem) -> str:       
+        return ocr.image_to_string(Image.open(input_imagem), lang="por")
+
+st.markdown(
+    """
+    ### Faça o upload de uma imagem :arrow_up:
+    """
+)
+
+input_imagem = st.file_uploader(
+    "Texto em formato de imagem",
+    type=["jpg", "jpeg", "png"],
+    accept_multiple_files=False,
+    label_visibility = "hidden"
+)
+
+if input_imagem is not None:
+    bytes_image = BytesIO(input_imagem.getvalue())
+    txt_file = convert_image_to_text(bytes_image)
+    st.success(f"Upload feito com sucesso!")
+    sleep(3)
+    st.markdown("""### Imagem convertida com sucesso!!! :sunglasses:""")
+    st.download_button(
+        label="Baixar Arquivo",
+        data=txt_file,
+        file_name="teste.txt",
+        key='download_button'
     )
 
